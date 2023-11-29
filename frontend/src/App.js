1bf89b33bfa83ab930da8c7a8fb5bc6a91ab2225
Aman Pandey <amanp.jun1906@gmail.com>
2023-11-29 15:40:09 +0530

    Navbar done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,31 @@
 
 import './App.css';
+import NavBar from './Components/NavBar/NavBar';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Shop from './Pages/Shop';
+import LoginSignup from './Pages/LoginSignup';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
 
 function App() {
   return (
     <div className="App">
-     
+    <BrowserRouter>
+    <NavBar />
+      <Routes>
+         <Route path='/' element={<Shop />}/>
+         <Route path='/login' element={<LoginSignup />}/>
+         <Route path='/mens' element={<ShopCategory category="men" />}/>
+         <Route path='/womens' element={<ShopCategory category="women"/>}/>
+         <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+         <Route path='/product' element={<Product />}>
+           <Route path=':productId' element={<Product />}/>
+         </Route>
+         <Route path='/cart' element={<Cart/>}/>
+      </Routes>
+    </BrowserRouter>
+    
     </div>
   );
 }
--- frontend/src/Components/NavBar/NavBar.jsx
+++ frontend/src/Components/NavBar/NavBar.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './NavBar.css';
+import logo from '../Assets/logo.png';
+import Badge from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Link } from 'react-router-dom';
+
 
 const NavBar = () => {
+
+    const[menu,setmenu] = useState('shop')
+
+    const StyledBadge = styled(Badge)(({ theme }) => ({
+        '& .MuiBadge-badge': {
+          right: -3,
+          top: 1,
+          border: `2px solid ${theme.palette.background.paper}`,
+          padding: '0 4px',
+        },
+      }));
+
   return (
-    <div>
-      
+    <div className='navbar'>
+    <div className='nav-logo'>
+       <img src={logo} alt="logo" />
+       <p>Craft_Buy</p>
+    </div>
+    <ul className='nav-menu'>
+        <li onClick={()=>{setmenu("shop")}}><Link to='/' className='link'>Shop</Link>{menu==="shop"?<hr />:<></>}</li>
+        <li onClick={()=>{setmenu("mens")}}><Link to='/mens' className='link'>Men</Link>{menu==="mens"?<hr />:<></>}</li>
+        <li onClick={()=>{setmenu("womens")}}><Link to='/womens' className='link'>Women</Link>{menu==="womens"?<hr />:<></>}</li>
+        <li onClick={()=>{setmenu("kids")}}><Link to='/kids' className='link'>Kids</Link>{menu==="kids"?<hr />:<></>}</li>
+    </ul>
+
+    <div className='navLoginCart'>
+    <button><Link to='/login' className='link'>Login</Link></button>
+    <Link className='link' to='/cart'>
+    <Badge>
+       <StyledBadge badgeContent={4} color="secondary">
+          <ShoppingCartIcon />
+       </StyledBadge>
+    </Badge>
+    </Link>
+    </div>
     </div>
   )
 }
